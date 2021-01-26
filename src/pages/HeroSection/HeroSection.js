ee61ff9a1bec682b088c12f636969dfcdf52f3ca
Muhammad Usama Zuberi <usama.zuberi1010@gmail.com>
2021-01-26 18:51:07 +0500

    Stories Added

--- src/pages/HeroSection/HeroSection.js
+++ src/pages/HeroSection/HeroSection.js
@@ -2,6 +2,8 @@ import React from 'react'
 import './HeroSection.scss'
 import billboardBG from '../../images/billboardBG.png';
 import Logo from '../../images/logo-black.png';
+import OnePlatform from '../../images/one-platform-for-all.jpg';
+import MoreGraphics from '../../images/more-graphics.jpg';
 import ButtonPoppins from "../../components/Button/ButtonPoppins";
 import FeaturesCard from '../../components/Cards/FeaturesCard';
 
@@ -43,9 +45,10 @@ function HeroSection() {
                     </div>
                 </div>
             </div>
+
             <div className="heroSec__features">
                 <div className="features">
-                    <h2 className="features__heading">
+                    <h2 className="heading-features">
                         Platform Features
                     </h2>
 
@@ -53,6 +56,23 @@ function HeroSection() {
                         <FeaturesCard />
                         <FeaturesCard />
                         <FeaturesCard />
+                    </div>
+                </div>
+            </div>
+
+            <div className="heroSec__stories">
+                <div className="stories">
+                    <h2 className="heading-features">One platform to manage them all</h2>
+                    <div className="stories__content">
+                        <img src={OnePlatform} alt="One of Pixtool's tab screenshot" className="stories__img stories__img--1" />
+                        <p className="stories__text stories__text--1">As designers, we know inspiration can make the difference between success and failure. Switching programs, converting files, waiting on hardware - these tasks can get in the way of a great idea. With pixtool you have all the tools you need to design, adjust and manage every element -  LED surfaces to stage layouts, weekend events to world tours. Pixtool has you covered.</p>
+                    </div>
+                </div>
+                <div className="stories">
+                    <h2 className="heading-features">One platform to manage them all</h2>
+                    <div className="stories__content">
+                        <img src={MoreGraphics} alt="A girl's face" className="stories__img stories__img--2" />
+                        <p className="stories__text stories__text--2">As designers, we know inspiration can make the difference between success and failure. Switching programs, converting files, waiting on hardware - these tasks can get in the way of a great idea. With pixtool you have all the tools you need to design, adjust and manage every element -  LED surfaces to stage layouts, weekend events to world tours. Pixtool has you covered.</p>
                     </div>
                 </div>
             </div>
