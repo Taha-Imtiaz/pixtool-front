a9288ae24bc3d40602980cf694f212dbb4a1f413
Muhammad Usama Zuberi <usama.zuberi1010@gmail.com>
2021-02-02 15:57:52 +0500

    WallStageLargeCard Designed

---
+++ src/components/Cards/WallStageLargeCard/WallStageLargeCard.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import './WallStageLargeCard.scss';
+import CoderWall from '../../../images/Icon simple-coderwall.png';
+import CoderWallBig from '../../../images/Icon simple-coderwall-2x.png';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+// import { createSlides } from "../utils/slides";
+
+function WallStageLargeCard() {
+
+    /* const createSlides = () => {} */
+
+    return (
+        <div className="wallStageLargeCard">
+            <h3 className="wallStageLargeCard__title">Wall Stage</h3>
+            <div className="wallStageLargeCard__imgBox">
+                <img src={CoderWallBig} alt="Coder Wall Big" className="wallStageLargeCard__largeImg" />
+
+                <div className="splider">
+                    <Splide
+                        options={{
+                            /* perPage: 2,
+                            perMove: 1,
+                            rewind: true, */
+                            autoHeight: true,
+                            autoWidth: true,
+                        }}
+                        onMoved={(splide, newIndex) => { console.log('moved', newIndex) }}
+                    >
+                        {/* {createSlides().map(slide => (
+                            <SplideSlide key={CoderWall.src}>
+                                <img src={CoderWall} alt="Coder Wall" className="wallStageLargeCard__smallImg" />
+                            </SplideSlide>
+                        ))} */}
+                        {/* {createSlides().map(slide => (
+                            <SplideSlide key={CoderWall.src}>
+                                <img src={CoderWall} alt="Coder Wall" className="wallStageLargeCard__smallImg" />
+                            </SplideSlide>
+                        ))}
+                        {createSlides().map(slide => (
+                            <SplideSlide key={CoderWall.src}>
+                                <img src={CoderWall} alt="Coder Wall" className="wallStageLargeCard__smallImg" />
+                            </SplideSlide>
+                        ))}
+                        {createSlides().map(slide => (
+                            <SplideSlide key={CoderWall.src}>
+                                <img src={CoderWall} alt="Coder Wall" className="wallStageLargeCard__smallImg" />
+                            </SplideSlide>
+                        ))}
+                        {createSlides().map(slide => (
+                            <SplideSlide key={CoderWall.src}>
+                                <img src={CoderWall} alt="Coder Wall" className="wallStageLargeCard__smallImg" />
+                            </SplideSlide>
+                        ))} */}
+
+                        <SplideSlide key={CoderWall.src}>
+                            <img src={CoderWall} alt="Coder Wall" className="wallStageLargeCard__smallImg" />
+                        </SplideSlide>
+
+                        <SplideSlide key={CoderWall.src}>
+                            <img src={CoderWall} alt="Coder Wall" className="wallStageLargeCard__smallImg" />
+                        </SplideSlide>
+
+                        <SplideSlide key={CoderWall.src}>
+                            <img src={CoderWall} alt="Coder Wall" className="wallStageLargeCard__smallImg" />
+                        </SplideSlide>
+
+                        <SplideSlide key={CoderWall.src}>
+                            <img src={CoderWall} alt="Coder Wall" className="wallStageLargeCard__smallImg" />
+                        </SplideSlide>
+
+                        <SplideSlide key={CoderWall.src}>
+                            <img src={CoderWall} alt="Coder Wall" className="wallStageLargeCard__smallImg" />
+                        </SplideSlide>
+                    </Splide>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default WallStageLargeCard
---
+++ src/components/ShowsComponents/Preview/Preview.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Preview.scss';
+import WallStageLargeCard from '../../Cards/WallStageLargeCard/WallStageLargeCard';
+
+function Preview() {
+    return (
+        <div className="preview">
+            <WallStageLargeCard />
+        </div>
+    )
+}
+
+export default Preview
