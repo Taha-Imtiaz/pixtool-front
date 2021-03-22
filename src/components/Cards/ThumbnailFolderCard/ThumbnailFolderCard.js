feae4dd0f07320dad2a784d07ff787dc229c7ef6
Muhammad Usama Zuberi <usama.zuberi1010@gmail.com>
2021-03-22 12:42:24 +0500

    FolderCardThumbnail Logic Correction

--- src/components/Cards/ThumbnailFolderCard/ThumbnailFolderCard.js
+++ src/components/Cards/ThumbnailFolderCard/ThumbnailFolderCard.js
@@ -7,71 +7,76 @@ import './ThumbnailFolderCard.scss';
 
 const ThumbnailFolderCard = ({ id, getAssets, resource }) => {
 
+    let srcArray = [];
+    for (let i = 0; i < 5; i++) {
+        srcArray.push('https://images.pexels.com/photos/6574738/pexels-photo-6574738.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940');
+    }
+    console.log(srcArray)
 
-const thumbnailsPic = 1;
-let {name,thumbnails} = resource
-// console.log(thumbnails.length)
+    let { name, thumbnails } = resource
+    // const thumbnailsLength = thumbnails.length;
+    const thumbnailsLength = srcArray.length;
+
+    console.log(thumbnails)
 
     return (
         <div className="thumbnailFolderCard" onClick={() => getAssets(id)}>
-            {thumbnails && thumbnails.map((thumbnailImg,index) =>
-                      <div 
-                      className={thumbnails.length <= 1 ? 'thumbnailFolderCard__thumbnailBox thumbnailFolderCard__thumbnailBox--1' : 'thumbnailFolderCard__thumbnailBox'}
-                      >
-              { thumbnails.length === 1 && <span className="thumbnailFolderCard__imgBox thumbnailFolderCard__imgBox--1">
-                    <img src={thumbnailImg}
+            {/* <div
+                className={thumbnailsLength <= 1 ? 'thumbnailFolderCard__thumbnailBox thumbnailFolderCard__thumbnailBox--1' : 'thumbnailFolderCard__thumbnailBox'}
+            >
+                {thumbnailsLength === 1 ? <span className="thumbnailFolderCard__imgBox thumbnailFolderCard__imgBox--1">
+                    <img src={srcArray[0]}
                         className="thumbnailFolderCard__img thumbnailFolderCard__img--1" />
                 </span>}
-              { thumbnails.length === 2 && <span className={thumbnails.length === 2 ? 'thumbnailFolderCard__imgBox thumbnailFolderCard__imgBox--2 thumbnailFolderCard__imgBox--expand' : 'thumbnailFolderCard__imgBox thumbnailFolderCard__imgBox--2'}>
-                    <img src={thumbnailImg}
+                {thumbnailsLength === 2 && <span className={thumbnailsLength === 2 ? 'thumbnailFolderCard__imgBox thumbnailFolderCard__imgBox--2 thumbnailFolderCard__imgBox--expand' : 'thumbnailFolderCard__imgBox thumbnailFolderCard__imgBox--2'}>
+                    <img src={srcArray[1]}
                         className="thumbnailFolderCard__img thumbnailFolderCard__img--2" />
                 </span>}
-                {thumbnails.length === 3 &&
+                {thumbnailsLength === 3 &&
                     <span className="thumbnailFolderCard__imgBox thumbnailFolderCard__imgBox--3">
-                        <img src={thumbnailImg}
+                        <img src={srcArray[2]}
                             className="thumbnailFolderCard__img thumbnailFolderCard__img--3" />
                     </span>
-                 
-                }
-                {
-                    thumbnails.length > 3 ?
-                        <span className="thumbnailFolderCard__imgBox thumbnailFolderCard__imgBox--3">
-                           {index < 2 ? <img src={thumbnailImg} alt="" /> : index === 2 ? <span> +{thumbnails.length-2}</span>:null}
-                            <span>+2</span>
-                        </span>
-                        : null
-                }
-                  
-               
-            </div>
-          )}
 
-{/* <div className={thumbnails <= 1 ? 'thumbnailFolderCard__thumbnailBox thumbnailFolderCard__thumbnailBox--1' : 'thumbnailFolderCard__thumbnailBox'}>
-                <span className="thumbnailFolderCard__imgBox thumbnailFolderCard__imgBox--1">
-                    <img src="https://images.pexels.com/photos/6574738/pexels-photo-6574738.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                        className="thumbnailFolderCard__img thumbnailFolderCard__img--1" />
-                </span>
-                <span className={thumbnails === 2 ? 'thumbnailFolderCard__imgBox thumbnailFolderCard__imgBox--2 thumbnailFolderCard__imgBox--expand' : 'thumbnailFolderCard__imgBox thumbnailFolderCard__imgBox--2'}>
-                    <img src="https://images.pexels.com/photos/6574738/pexels-photo-6574738.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                        className="thumbnailFolderCard__img thumbnailFolderCard__img--2" />
-                </span>
-                {thumbnails === 3 ?
-                    <span className="thumbnailFolderCard__imgBox thumbnailFolderCard__imgBox--3">
-                        <img src="https://images.pexels.com/photos/6574738/pexels-photo-6574738.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                            className="thumbnailFolderCard__img thumbnailFolderCard__img--3" />
-                    </span>
-                    : null
                 }
                 {
-                    thumbnails > 3 ?
+                    thumbnailsLength > 3 ?
                         <span className="thumbnailFolderCard__imgBox thumbnailFolderCard__imgBox--3">
+                            {index < 2 ? <img src={thumbnailImg} alt="" /> : index === 2 ? <span> +{thumbnailsLength-2}</span>:null}
                             <span>+2</span>
                         </span>
                         : null
                 }
+
+
             </div> */}
-        
-      
+
+            <div className={thumbnailsLength <= 1 ? 'thumbnailFolderCard__thumbnailBox thumbnailFolderCard__thumbnailBox--1' : 'thumbnailFolderCard__thumbnailBox'}>
+                <span className="thumbnailFolderCard__imgBox thumbnailFolderCard__imgBox--1">
+                    <img src={srcArray[0]}
+                        className="thumbnailFolderCard__img thumbnailFolderCard__img--1" />
+                </span>
+                <span className={thumbnailsLength === 2 ? 'thumbnailFolderCard__imgBox thumbnailFolderCard__imgBox--2 thumbnailFolderCard__imgBox--expand' : 'thumbnailFolderCard__imgBox thumbnailFolderCard__imgBox--2'}>
+                    <img src={srcArray[1]}
+                        className="thumbnailFolderCard__img thumbnailFolderCard__img--2" />
+                </span>
+                {thumbnailsLength === 3 ?
+                    <span className="thumbnailFolderCard__imgBox thumbnailFolderCard__imgBox--3">
+                        <img src={srcArray[2]} className="thumbnailFolderCard__img thumbnailFolderCard__img--3" />
+                    </span>
+                    :
+                    null
+                }
+                {thumbnailsLength > 3 ?
+                    <span className="thumbnailFolderCard__imgBox thumbnailFolderCard__imgBox--3">
+                        <span>+2</span>
+                    </span>
+                    :
+                    null
+                }
+            </div>
+
+
             {/* {thumbnails && thumbnails.map((thumbnailImg,index) =>
                 <div className={thumbnailsPic <= 1 ? 'thumbnailFolderCard__thumbnailBox thumbnailFolderCard__thumbnailBox--1' : 'thumbnailFolderCard__thumbnailBox'}>
            { thumbnailsPic ===1 ?  
@@ -92,9 +97,9 @@ let {name,thumbnails} = resource
                       </div>
                     )}
                */}
-               
-               
-          
+
+
+
             <div className="thumbnailFolderCard__text">
                 <div className="thumbnailFolderCard__name truncate">{name}</div>
                 <div className="thumbnailFolderCard__datail">
