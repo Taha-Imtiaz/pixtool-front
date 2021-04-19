35285af1689870f290275c2ae6f6c3977fff1bd2
Muhammad Usama Zuberi <usama.zuberi1010@gmail.com>
2021-04-19 16:49:30 +0500

    Review Page Designed

--- src/pages/Review/Review.js
+++ src/pages/Review/Review.js
@@ -1,72 +1,35 @@
-import React from "react";
-import { Fragment } from "react";
+import React, {Fragment} from "react";
 import { connect } from "react-redux";
-import ThumbnailCard from "../../components/Cards/ThumbnailCard/ThumbnailCard";
-import ThumbnailFolderCard from "../../components/Cards/ThumbnailFolderCard/ThumbnailFolderCard";
-import Header from "../../components/Header/Header";
-import NoDataFoundImg from "../../images/no-data-found.png";
 
-const Review = ({ resources }) => {
+import './Review.scss';
+
+import Header from "../../components/Header/Header";
+// import ThumbnailCard from "../../components/Cards/ThumbnailCard/ThumbnailCard";
+// import ThumbnailFolderCard from "../../components/Cards/ThumbnailFolderCard/ThumbnailFolderCard";
+
+// import NoDataFoundImg from "../../images/no-data-found.png";
+
+
+const Review = () => {
+
+
   return (
     <Fragment>
       <Header className="header" />
-      <div className="library">
-        <div className="library__head">
-          {/* <Filter /> */}
-          <div className="library__head-right">
-            <div className="avatar__container">
-              <div className="avatar">
-                {/* <i className="fas fa-user-plus"></i> */}
-              </div>
-              {/* <Avatar />
-              <Avatar /> */}
-            </div>
+      <div className="review">
 
-            <div className="library__head__buttons">
-              {/* <Button text="Share" click={shareLibrary} />
-              <Dropdown text="New" menuItems={newUpload} /> */}
-            </div>
-          </div>
+        <div className="review__gallery">
+
         </div>
-
-
-      <div className="library__main">
-        <div className="thumbnail-container">
-          {resources &&
-            (resources.length > 0 ? (
-              resources.map((resource, index) =>
-                resource._type === "file" ? (
-                  <ThumbnailCard
-                  // shareAssetIds={shareAssetIds}
-                  // setAssetIds={setAssetIds}
-
-                  // key={resource._id}
-                  // id={resource._id}
-                  // resource={resource}
-                  // showCheckbox={showCheckbox}
-                  // index={index}
-                  />
-                ) : (
-                  <ThumbnailFolderCard
-                  // key={resource._id} id={resource._id} resource={resource} shareAssetIds={shareAssetIds} setAssetIds={setAssetIds} showCheckbox={showCheckbox}
-                  />
-                )
-              )
-            ) : (
-              <img
-                src={NoDataFoundImg}
-                alt="No Data Found"
-                className="margin-auto"
-              />
-            ))}
-        </div>
-      </div>
-      </div>
+      </div >
     </Fragment>
   );
 };
+
+
 var mapStateToProps = (state) => ({
   resources: state.project && state.project.resources,
 });
+
 
 export default connect(mapStateToProps)(Review);
