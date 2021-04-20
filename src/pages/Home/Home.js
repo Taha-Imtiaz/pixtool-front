2b7fcc70ddf8499d3417e736bdc927d6aa04be95
Taha-Imtiaz <tahaimtiaz1996@gmail.com>
2021-04-20 09:21:32 +0500

    push code

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,222 +1,223 @@
-import React, { useState } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { connect } from 'react-redux';
+import React, { useState } from "react";
+import { Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
 
-import './Home.scss'
+import "./Home.scss";
 
-import Tabs from '../../components/NavigationTabs/Tabs';
-import InnerTabs from '../../components/InnerNav/InnerTabs';
-import Header from '../../components/Header/Header';
-import Sidebar from '../../components/Sidebar/Sidebar';
-import TeamsNav from '../../components/TeamsNav/TeamsNav'
-import Library from '../../components/Library/Library';
-import Overview from '../../components/ShowsComponents/Overview/Overview';
-import Settings from '../../components/ShowsComponents/Settings/Settings';
-import Media from '../../components/ShowsComponents/Media/Media';
-import Preview from '../../components/ShowsComponents/Preview/Preview';
-import Export from '../../components/ShowsComponents/Export/Export';
-import Surfaces from '../../components/StagesComponents/Surfaces/Surfaces';
-import Screens from '../../components/StagesComponents/Screens/Screens';
-import Stages from '../../components/StagesComponents/Stages/Stages';
+import Tabs from "../../components/NavigationTabs/Tabs";
+import InnerTabs from "../../components/InnerNav/InnerTabs";
+import Header from "../../components/Header/Header";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import TeamsNav from "../../components/TeamsNav/TeamsNav";
+import Library from "../../components/Library/Library";
+import Overview from "../../components/ShowsComponents/Overview/Overview";
+import Settings from "../../components/ShowsComponents/Settings/Settings";
+import Media from "../../components/ShowsComponents/Media/Media";
+import Preview from "../../components/ShowsComponents/Preview/Preview";
+import Export from "../../components/ShowsComponents/Export/Export";
+import Surfaces from "../../components/StagesComponents/Surfaces/Surfaces";
+import Screens from "../../components/StagesComponents/Screens/Screens";
+import Stages from "../../components/StagesComponents/Stages/Stages";
 
 // Modal Imports
-import AddProjectModal from '../../components/Modals/AddProjectModal/AddProjectModal';
-import AddFolderModal from '../../components/Modals/AddFolderModal/AddFolderModal';
-import ShareModal from '../../components/Modals/ShareModal/ShareModal';
-import ProjectOptionsModal from '../../components/Modals/ProjectOptionsModal/ProjectOptionsModal';
-
-
-
+import AddProjectModal from "../../components/Modals/AddProjectModal/AddProjectModal";
+import AddFolderModal from "../../components/Modals/AddFolderModal/AddFolderModal";
+import ShareModal from "../../components/Modals/ShareModal/ShareModal";
+import ProjectOptionsModal from "../../components/Modals/ProjectOptionsModal/ProjectOptionsModal";
 
 function Home({ project, match: { path } }) {
+  // This state is used to set teamId
+  const [teamId, setTeamId] = useState(null);
 
-    // This state is used to set teamId
-    const [teamId, setTeamId] = useState(null)
+  // This state is used to Show/ Hide the AddProjectModal
+  const [showAddProjectModal, setShowAddProjectModal] = useState(false);
+  // This state is used to Show/ Hide the AddFolderModal
+  const [showAddFolderModal, setShowAddFolderModal] = useState(false);
+  // This state is used to Show/ Hide the ShareModal
+  const [showShareModal, setShowShareModal] = useState(false);
+  // This state is used to Show/ Hide ProjectOptionsModal
+  const [showProjectMenu, setShowProjectMenu] = useState(false);
 
+  // This is State is used to Show/ Hide the Share Checkbox On ThumbnailCard
+  const [showCheckbox, setShowCheckbox] = useState(false);
 
-    // This state is used to Show/ Hide the AddProjectModal
-    const [showAddProjectModal, setShowAddProjectModal] = useState(false);
-    // This state is used to Show/ Hide the AddFolderModal
-    const [showAddFolderModal, setShowAddFolderModal] = useState(false);
-    // This state is used to Show/ Hide the ShareModal
-    const [showShareModal, setShowShareModal] = useState(false);
-    // This state is used to Show/ Hide ProjectOptionsModal
-    const [showProjectMenu, setShowProjectMenu] = useState(false);
+  // This Function is responsible to Show/ Hide the AddProjectModal
+  const addProjectModalToggle = (e, teamId) => {
+    setTeamId(teamId);
+    // prevent parent component to render when open open a modal
+    e.stopPropagation();
+    if (showAddProjectModal) {
+      // Allow to scroll when closing the modal
+      document.body.style.removeProperty("overflow");
+    } else {
+      // Disable scrolling on the `body` element when opening a modal
+      document.body.style.overflow = "hidden";
+    }
 
+    setShowAddProjectModal(!showAddProjectModal);
+  };
 
-    // This is State is used to Show/ Hide the Share Checkbox On ThumbnailCard
-    const [showCheckbox, setShowCheckbox] = useState(false);
+  // This Function is responsible to Show/ Hide the AddFolderModal
+  const addFolderModalToggle = () => {
+    if (showAddFolderModal) {
+      // Allow to scroll when closing the modal
+      document.body.style.removeProperty("overflow");
+    } else {
+      // Disable scrolling on the `body` element when opening a modal
+      document.body.style.overflow = "hidden";
+    }
 
+    setShowAddFolderModal(!showAddFolderModal);
+  };
 
+  // This Function is responsible to Show/ Hide the ShareModal
+  const shareModalToggle = () => {
+    if (showShareModal) {
+      // Allow to scroll when closing the modal
+      document.body.style.removeProperty("overflow");
+    } else {
+      // Disable scrolling on the `body` element when opening a modal
+      document.body.style.overflow = "hidden";
+    }
 
+    setShowShareModal(!showShareModal);
+  };
 
-    // This Function is responsible to Show/ Hide the AddProjectModal
-    const addProjectModalToggle = (e, teamId) => {
-        setTeamId(teamId)
-        // prevent parent component to render when open open a modal
-        e.stopPropagation()
-        if (showAddProjectModal) {
-            // Allow to scroll when closing the modal
-            document.body.style.removeProperty('overflow');
+  // This Function is responsible to Show/ Hide the ProjectOptionsModal
+  const projectOptionsModalToggle = () => {
+    if (showProjectMenu) {
+      // Allow to scroll when closing the modal
+      document.body.style.removeProperty("overflow");
+    } else {
+      // Disable scrolling on the `body` element when opening a modal
+      document.body.style.overflow = "hidden";
+    }
 
-        } else {
-            // Disable scrolling on the `body` element when opening a modal
-            document.body.style.overflow = 'hidden';
-        }
+    setShowProjectMenu(false);
+  };
 
-        setShowAddProjectModal(!showAddProjectModal);
-    };
+  /* Arrays To Pass The Sidebar Content In Props */
+  let sidebarMenu1 = [
+    { icon: "fas fa-book", value: "My Library" },
+    { icon: "far fa-file-alt", value: "Shared with me" },
+    { icon: "fas fa-cog", value: "Settings" },
+  ];
 
-    // This Function is responsible to Show/ Hide the AddFolderModal
-    const addFolderModalToggle = () => {
+  return (
+    <div className="home page-wrapper">
+      {/* Above "page-wrapper" class is added only to tell dropdowns that it is the main wrapper and to make them function properly */}
 
-        if (showAddFolderModal) {
-            // Allow to scroll when closing the modal
-            document.body.style.removeProperty('overflow');
+      <Sidebar menu1={sidebarMenu1}>
+        <TeamsNav
+          showAddProjectModal={showAddProjectModal}
+          addProjectModalToggle={addProjectModalToggle}
+          showProjectMenu={showProjectMenu}
+          setShowProjectMenu={setShowProjectMenu}
+        />
+      </Sidebar>
 
-        } else {
-            // Disable scrolling on the `body` element when opening a modal
-            document.body.style.overflow = 'hidden';
-        }
+      <div>
+        <Header className="header" />
 
-        setShowAddFolderModal(!showAddFolderModal);
-    };
-
-    // This Function is responsible to Show/ Hide the ShareModal
-    const shareModalToggle = () => {
-
-        if (showShareModal) {
-            // Allow to scroll when closing the modal
-            document.body.style.removeProperty('overflow');
-
-        } else {
-            // Disable scrolling on the `body` element when opening a modal
-            document.body.style.overflow = 'hidden';
-        }
-
-        setShowShareModal(!showShareModal);
-    };
-
-    // This Function is responsible to Show/ Hide the ProjectOptionsModal
-    const projectOptionsModalToggle = () => {
-
-        if (showProjectMenu) {
-            // Allow to scroll when closing the modal
-            document.body.style.removeProperty('overflow');
-
-        } else {
-            // Disable scrolling on the `body` element when opening a modal
-            document.body.style.overflow = 'hidden';
-        }
-
-        setShowProjectMenu(false);
-    };
-
-    /* Arrays To Pass The Sidebar Content In Props */
-    let sidebarMenu1 = [
-        { icon: 'fas fa-book', value: 'My Library' },
-        { icon: 'far fa-file-alt', value: 'Shared with me' },
-        { icon: 'fas fa-cog', value: 'Settings' }
-    ];
-
-
-    return (
-        <div className="home page-wrapper">
-            {/* Above "page-wrapper" class is added only to tell dropdowns that it is the main wrapper and to make them function properly */}
-
-            <Sidebar menu1={sidebarMenu1}>
-                <TeamsNav
-                    showAddProjectModal={showAddProjectModal} addProjectModalToggle={addProjectModalToggle}
-                    showProjectMenu={showProjectMenu} setShowProjectMenu={setShowProjectMenu}
+        {/* This is home's main tab which include Library, Shows & Stages */}
+        {project && (
+          <Tabs className="tabs">
+            <Switch>
+              <Route path={`${path}/library/${project._id}`} label="Library">
+                <Library
+                  addFolderModalToggle={addFolderModalToggle}
+                  shareModalToggle={shareModalToggle}
+                  showCheckbox={showCheckbox}
+                  setShowCheckbox={setShowCheckbox}
                 />
-            </Sidebar>
+              </Route>
+              <Route path={`${path}/shows/${project._id}`} label="Shows">
+                <InnerTabs>
+                  {/* Shows - Overview Tab Content */}
+                  <div label="Overview">
+                    <Overview />
+                  </div>
 
-            <div>
-                <Header className="header" />
+                  {/* Shows - Settings Tab Content */}
+                  <div label="Settings">
+                    <Settings />
+                  </div>
 
-                {/* This is home's main tab which include Library, Shows & Stages */}
-                {project && <Tabs className="tabs">
+                  {/*Shows - Media Tab Content */}
+                  <div label="Media">
+                    <Media />
+                  </div>
 
-                    <Switch>
-                        <Route path={`${path}/library/${project._id}`} label="Library" >
-                            <Library
-                                addFolderModalToggle={addFolderModalToggle} shareModalToggle={shareModalToggle}
-                                showCheckbox={showCheckbox} setShowCheckbox={setShowCheckbox} />
-                        </Route>
-                        <Route path={`${path}/shows/${project._id}`} label="Shows" >
-                            <InnerTabs>
-                                {/* Shows - Overview Tab Content */}
-                                <div label="Overview">
-                                    <Overview />
-                                </div>
+                  {/*Shows -  Preview Tab Content */}
+                  <div label="Preview">
+                    <Preview />
+                  </div>
 
-                                {/* Shows - Settings Tab Content */}
-                                <div label="Settings">
-                                    <Settings />
-                                </div>
+                  {/*Shows - Export Tab Content */}
+                  <div label="Export">
+                    <Export />
+                  </div>
+                </InnerTabs>
+              </Route>
+              <Route path={`${path}/stages/${project._id}`} label="Stages">
+                <InnerTabs>
+                  {/* Stages - Surfaces Tab Content */}
+                  <div label="Surfaces">
+                    <Surfaces />
+                  </div>
 
-                                {/*Shows - Media Tab Content */}
-                                <div label="Media">
-                                    <Media />
-                                </div>
+                  {/* Stages - Screens Tab Content */}
+                  <div label="Screens">
+                    <Screens />
+                  </div>
 
-                                {/*Shows -  Preview Tab Content */}
-                                <div label="Preview">
-                                    <Preview />
-                                </div>
+                  {/*Stages - Stages Tab Content */}
+                  <div label="Stages">
+                    <Stages />
+                  </div>
+                </InnerTabs>
+              </Route>
+            </Switch>
+          </Tabs>
+        )}
+      </div>
 
-                                {/*Shows - Export Tab Content */}
-                                <div label="Export">
-                                    <Export />
-                                </div>
-                            </InnerTabs>
-                        </Route>
-                        <Route path={`${path}/stages/${project._id}`} label="Stages" >
-                            <InnerTabs>
-                                {/* Stages - Surfaces Tab Content */}
-                                <div label="Surfaces">
-                                    <Surfaces />
-                                </div>
+      {/* All The Modal Components Used In Home Page, All It's Tabs & Sidebar*/}
 
-                                {/* Stages - Screens Tab Content */}
-                                <div label="Screens">
-                                    <Screens />
-                                </div>
+      {/* This is AddProjectModal */}
+      <AddProjectModal
+        showModal={showAddProjectModal}
+        setShowModal={setShowAddProjectModal}
+        modalToggler={addProjectModalToggle}
+        teamId={teamId}
+      />
 
-                                {/*Stages - Stages Tab Content */}
-                                <div label="Stages">
-                                    <Stages />
-                                </div>
-                            </InnerTabs>
-                        </Route>
-                    </Switch>
-                </Tabs>
+      {/* This is AddNewFolderModal */}
+      <AddFolderModal
+        showModal={showAddFolderModal}
+        setShowModal={setShowAddFolderModal}
+        modalToggler={addFolderModalToggle}
+      />
 
-                }
-            </div>
+      {/* This is ShareModal */}
+      <ShareModal
+        showModal={showShareModal}
+        setShowModal={setShowShareModal}
+        modalToggler={shareModalToggle}
+      />
 
-            {/* All The Modal Components Used In Home Page, All It's Tabs & Sidebar*/}
-
-            {/* This is AddProjectModal */}
-            <AddProjectModal showModal={showAddProjectModal} setShowModal={setShowAddProjectModal} modalToggler={addProjectModalToggle} teamId={teamId} />
-
-            {/* This is AddNewFolderModal */}
-            <AddFolderModal showModal={showAddFolderModal} setShowModal={setShowAddFolderModal} modalToggler={addFolderModalToggle} />
-
-            {/* This is ShareModal */}
-            <ShareModal showModal={showShareModal} setShowModal={setShowShareModal} modalToggler={shareModalToggle} />
-
-            {/* This is ProjectOptionsModal */}
-            <ProjectOptionsModal showModal={showProjectMenu} setShowModal={setShowProjectMenu} modalToggler={projectOptionsModalToggle} />
-
-        </div>
-    )
+      {/* This is ProjectOptionsModal */}
+      <ProjectOptionsModal
+        showModal={showProjectMenu}
+        setShowModal={setShowProjectMenu}
+        modalToggler={projectOptionsModalToggle}
+      />
+    </div>
+  );
 }
 
 var mapStateToProps = (state) => ({
-    project: state.project
-})
+  project: state.project,
+});
 
-
-
-export default connect(mapStateToProps)(Home)
+export default connect(mapStateToProps)(Home);
--- src/pages/Review/Review.js
+++ src/pages/Review/Review.js
@@ -1,35 +1,72 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 import { connect } from "react-redux";
 
-import './Review.scss';
+import "./Review.scss";
 
 import Header from "../../components/Header/Header";
+import ThumbnailCard from "../../components/Cards/ThumbnailCard/ThumbnailCard";
+import ThumbnailFolderCard from "../../components/Cards/ThumbnailFolderCard/ThumbnailFolderCard";
 // import ThumbnailCard from "../../components/Cards/ThumbnailCard/ThumbnailCard";
 // import ThumbnailFolderCard from "../../components/Cards/ThumbnailFolderCard/ThumbnailFolderCard";
 
-// import NoDataFoundImg from "../../images/no-data-found.png";
+import NoDataFoundImg from "../../images/no-data-found.png";
 
-
-const Review = () => {
-
-
+const Review = ({ user, resources }) => {
   return (
     <Fragment>
       <Header className="header" />
       <div className="review">
+        <div className="review__header">
+          <h3>{`Review Link ${new Date().toDateString()}`} </h3>
+          {user && (
+            <h3 className="review__header__username">{`Shared By ${user.full_name}`}</h3>
+          )}
+          <div className="review__header__horizontal-line"></div>
+        </div>
 
         <div className="review__gallery">
+            
+          {resources &&
+            (resources.length > 0 ? (
+              resources.map((resource, index) =>
+                resource._type === "file" ? (
+                  <ThumbnailCard
+                  // shareAssetIds={shareAssetIds}
+                  // setAssetIds={setAssetIds}
 
+                  key={resource._id}
+                  id={resource._id}
+                  resource={resource}
+                  // showCheckbox={showCheckbox}
+                  // index={index}
+                  />
+                ) : (
+                  <ThumbnailFolderCard
+                    key={resource._id}
+                    id={resource._id}
+                    resource={resource}
+                    // shareAssetIds={shareAssetIds}
+                    // setAssetIds={setAssetIds}
+                    // showCheckbox={showCheckbox}
+                  />
+                )
+              )
+            ) : (
+              <img
+                src={NoDataFoundImg}
+                alt="No Data Found"
+                className="margin-auto"
+              />
+            ))}
         </div>
-      </div >
+      </div>
     </Fragment>
   );
 };
 
-
 var mapStateToProps = (state) => ({
   resources: state.project && state.project.resources,
+  user: state.users && state.users.user,
 });
-
 
 export default connect(mapStateToProps)(Review);
