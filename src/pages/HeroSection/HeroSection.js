d605d78191981f9a1743fe7095c70fc5f45ccfc0
Muhammad Usama Zuberi <usama.zuberi1010@gmail.com>
2021-05-10 14:23:37 +0500

    Hero Section Responsiveness FIxed

--- src/pages/HeroSection/HeroSection.js
+++ src/pages/HeroSection/HeroSection.js
@@ -63,10 +63,12 @@ function HeroSection({ history }) {
             <div className="heroSec__billBoard">
                 <div className="billBoard">
                     <div className="billBoard__left">
-                        <span className="billBoard__text">
-                            Best Video 3D <span className="billBoard__text--primary">Editing</span> Tool
-                            <ButtonPoppins text="Explore" click={createNew} />
-                        </span>
+                        <p className="billBoard__text">
+                            Best Video 3D
+                            <span className="billBoard__text--primary">&nbsp;Editing&nbsp;</span> 
+                            Tool
+                        </p>
+                        <ButtonPoppins text="Explore" click={createNew} />
                     </div>
                     <div className="billBoard__right">
                         <img src={billboardBG} alt="An illustration showing video editing" className="billBoard__img" />
@@ -93,14 +95,14 @@ function HeroSection({ history }) {
                     <h2 className="features__heading">One platform to manage them all</h2>
                     <div className="stories__content">
                         <img src={OnePlatform} alt="One of Pixtool's tab screenshot" className="stories__img stories__img--1" />
-                        <p className="stories__text stories__text--1">As designers, we know inspiration can make the difference between success and failure. Switching programs, converting files, waiting on hardware - these tasks can get in the way of a great idea. With pixtool you have all the tools you need to design, adjust and manage every element -  LED surfaces to stage layouts, weekend events to world tours. Pixtool has you covered.</p>
+                        <p className="stories__text">As designers, we know inspiration can make the difference between success and failure. Switching programs, converting files, waiting on hardware - these tasks can get in the way of a great idea. With pixtool you have all the tools you need to design, adjust and manage every element -  LED surfaces to stage layouts, weekend events to world tours. Pixtool has you covered.</p>
                     </div>
                 </div>
                 <div className="stories">
                     <h2 className="features__heading">One platform to manage them all</h2>
                     <div className="stories__content">
                         <img src={MoreGraphics} alt="A girl's face" className="stories__img stories__img--2" />
-                        <p className="stories__text stories__text--2">As designers, we know inspiration can make the difference between success and failure. Switching programs, converting files, waiting on hardware - these tasks can get in the way of a great idea. With pixtool you have all the tools you need to design, adjust and manage every element -  LED surfaces to stage layouts, weekend events to world tours. Pixtool has you covered.</p>
+                        <p className="stories__text">As designers, we know inspiration can make the difference between success and failure. Switching programs, converting files, waiting on hardware - these tasks can get in the way of a great idea. With pixtool you have all the tools you need to design, adjust and manage every element -  LED surfaces to stage layouts, weekend events to world tours. Pixtool has you covered.</p>
                     </div>
                 </div>
             </div>
